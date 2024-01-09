e84d8c27a28689f6ef923a1c81e2ebbd87005bfb
KisingiMichael <kisingimichael12@gmail.com>
2024-01-09 06:39:09 -0800

    fix errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,12 +5,6 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
 
 
 @app_views.route("/status", strict_slashes=False, methods=["GET"])
